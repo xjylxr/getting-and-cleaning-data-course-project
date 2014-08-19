f8c68b9d98e5b2888aecaf13e91893d9b2982ca3
xjylxr <xjylxr@users.noreply.github.com>
2014-08-19 14:04:06 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+# Step1. Merges the training and the test sets to create one data set.
 trainData <- read.table("./data/train/X_train.txt")
 dim(trainData)
 head(trainData)
@@ -16,6 +17,7 @@ dim(joinLabel)
 joinSubject <- rbind(trainSubject, testSubject)
 dim(joinSubject)
 
+# Step2. Extracts only the measurements on the mean and standard deviation for each measurement.
 features <- read.table("./data/features.txt")
 dim(features)
 meanStdIndices <- grep("mean\\(\\)|std\\(\\)", features[, 2])
@@ -27,6 +29,7 @@ names(joinData) <- gsub("mean", "Mean", names(joinData)) # capitalize M
 names(joinData) <- gsub("std", "Std", names(joinData)) # capitalize S
 names(joinData) <- gsub("-", "", names(joinData)) # remove "-" in column names 
 
+# Step3. Uses descriptive activity names to name the activities in the data set
 activity <- read.table("./data/activity_labels.txt")
 activity[, 2] <- tolower(gsub("_", "", activity[, 2]))
 substr(activity[2, 2], 8, 8) <- toupper(substr(activity[2, 2], 8, 8))
@@ -35,11 +38,13 @@ activityLabel <- activity[joinLabel[, 1], 2]
 joinLabel[, 1] <- activityLabel
 names(joinLabel) <- "activity"
 
+# Step4. Appropriately labels the data set with descriptive activity names.
 names(joinSubject) <- "subject"
 cleanedData <- cbind(joinSubject, joinLabel, joinData)
 dim(cleanedData)
 write.table(cleanedData, "merged_data.txt")
 
+# Step5. Creates a second, independent tidy data set
 subjectLen <- length(table(joinSubject))
 activityLen <- dim(activity)[1] 
 columnLen <- dim(cleanedData)[2]
